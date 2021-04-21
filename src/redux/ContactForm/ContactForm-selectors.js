fb05379abb25a3c1f44131596938946b16845fa7
Tetiana Lavrenko <tet.lavrenko@gmail.com>
2021-04-21 23:28:34 +0200

    added createSelector

--- src/redux/ContactForm/ContactForm-selectors.js
+++ src/redux/ContactForm/ContactForm-selectors.js
@@ -4,14 +4,21 @@ const getFilter = state => state.contacts.filter;
 
 const getItems = state => state.contacts.items;
 
-const getContacts = state => {
-  const filter = getFilter(state);
-  const items = getItems(state);
+// const getContacts = state => {
+//   const filter = getFilter(state);
+//   const items = getItems(state);
 
+//   const normalizedFilter = filter.toLowerCase();
+//   return items.filter(({ name }) =>
+//     name ? name.toLowerCase().includes(normalizedFilter) : false,
+//   );
+// };
+
+const getContacts = createSelector([getFilter, getItems], (filter, items) => {
   const normalizedFilter = filter.toLowerCase();
   return items.filter(({ name }) =>
     name ? name.toLowerCase().includes(normalizedFilter) : false,
   );
-};
+});
 
 export default { getFilter, getContacts };
